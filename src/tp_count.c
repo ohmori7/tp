a5300911fa7abb29387c04a4d8d55d061495fa86
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2023-06-10 16:34:12 +0900

    preparation for specifying an interval to output statistics.

--- src/tp_count.c
+++ src/tp_count.c
@@ -10,6 +10,7 @@ tp_count_init(struct tp_count *tpc, const char *desc)
 	tpc->tpc_count = 0;
 	tpc->tpc_bytes = 0;
 	tpc->tpc_errors = 0;
+	tpc->tpc_interval = TP_COUNT_DEFAULT_INTERVAL;
 }
 
 void
@@ -24,8 +25,7 @@ tp_count_inc(struct tp_count *tpc, size_t bytes)
 	tpc->tpc_count++;
 	tpc->tpc_bytes += bytes;
 
-#define TP_COUNT_THRESHOLD	10000
-	if (tpc->tpc_count % TP_COUNT_THRESHOLD == 0)
+	if (tpc->tpc_count % tpc->tpc_interval == 0)
 		fprintf(stderr, "%s %zu (%zu bytes, %zu errors)\n",
 		    tpc->tpc_desc, tpc->tpc_count, tpc->tpc_bytes, tpc->tpc_errors);
 }
--- src/tp_count.h
+++ src/tp_count.h
@@ -3,6 +3,8 @@ struct tp_count {
 	size_t tpc_count;
 	size_t tpc_bytes;
 	size_t tpc_errors;
+#define TP_COUNT_DEFAULT_INTERVAL	500000	/* unit in packet */
+	size_t tpc_interval;
 };
 
 void tp_count_init(struct tp_count *, const char *);
